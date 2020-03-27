6bc86ecbd2cb5fb2898baf62aff95585bca38074
Jonghyun Lee <ahamaker@users.noreply.github.com>
2020-03-28 00:01:03 +0900

    3주차 과제

---
+++ crawling_project/music_rank.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+
+# 타겟 URL을 읽어서 HTML를 받아오고,
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200309',headers=headers)
+
+# HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
+# soup이라는 변수에 "파싱 용이해진 html"이 담긴 상태가 됨
+# 이제 코딩을 통해 필요한 부분을 추출하면 된다.
+soup = BeautifulSoup(data.text, 'html.parser')
+
+# select를 이용해서, tr들을 불러오기
+musics = soup.select('tr.list')
+
+# musics(tr들)의 반복문을 돌리기
+rank = 0
+for music in musics:
+    if rank <= len(musics):
+        rank += 1
+        title = music.select_one('td.info > a.title.ellipsis').text
+        singer = music.select_one('td.info > a.artist.ellipsis').text
+        title = title.strip()
+        print(rank, title, singer)
+
